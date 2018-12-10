abde26b19d786e0b311aff0b7b79ce74cb84962a
Katherine Sawyer <katherine.a.sawyer@gmail.com>
2018-12-09 19:05:36 -0800

    Updates to React config

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,13 +1,8 @@
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
-<<<<<<< HEAD
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
 // vendor/assets/javascripts directory can be referenced here using a relative path.
-=======
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
->>>>>>> a3925c98e2a97a7384832460ef42974b50091bf2
 //
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // compiled file. JavaScript code in this file should be added after the last require_* statement.
@@ -17,12 +12,5 @@
 //
 //= require jquery
 //= require jquery_ujs
-<<<<<<< HEAD
-//= require foundation
 //= require activestorage
 //= require_tree .
-
-$(function(){ $(document).foundation(); });
-=======
-//= require_tree .
->>>>>>> a3925c98e2a97a7384832460ef42974b50091bf2
