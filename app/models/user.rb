2a8718efc125fd41b7825e206ae6e9b0cb567e5d
Katherine Sawyer <katherine.a.sawyer@gmail.com>
2018-12-18 23:02:26 -0800

    updating documentation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
-# Users managed with devise
+# Users managed with devise authentication
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
